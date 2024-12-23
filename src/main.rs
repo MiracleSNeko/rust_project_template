1aa78cd302effbf0521a043ce503397b2faf4837
MiracleSNeko <miracle.neko@qq.com>
2024-12-23 07:46:07 +0000

    ci(ci): add merge ci pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn test1() {
+    assert_eq!(add(40, 2), 42);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(String::from("40") + "2", String::from("402"));
+}
