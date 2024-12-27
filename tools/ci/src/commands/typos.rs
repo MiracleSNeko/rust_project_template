828ac2f69d42faa150598cb212019dee855c4f1f
MiracleSNeko <miracle.neko@qq.com>
2024-12-27 09:32:09 +0000

    ci(commands): fix typo in typo command XD
    Signed-off-by: MiracleSNeko <miracle.neko@qq.com>

--- tools/ci/src/commands/typos.rs
+++ tools/ci/src/commands/typos.rs
@@ -10,7 +10,7 @@ pub struct TypoCommand {}
 impl Run for TypoCommand {
     fn run<'a>(&self, shell: &'a Shell, _flags: RunFlags) -> Vec<RunnableCommand<'a>> {
         vec![RunnableCommand::new::<Self>(
-            cmd!(shell, "typo"),
+            cmd!(shell, "typos"),
             String::from("seems like there are some typos in your code , please fix errors above/below and try again."),
         )]
     }
